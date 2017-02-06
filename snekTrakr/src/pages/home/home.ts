63f439dc39cba8e7894d6c7dab451476e6df996d
Charles Stafford <j.stafford1992@gmail.com>
2017-02-06 16:21:41 -0700

    working on dependency issues

--- snekTrakr/src/pages/home/home.ts
+++ snekTrakr/src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { UserAuth } from '../../providers/user-auth';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -11,21 +11,37 @@ export class HomePage {
 
   cred = {email: '', password: ''};
 
-  constructor(public navCtrl: NavController, private auth: UserAuth ) {
+  constructor(public navCtrl: NavController,
+    private auth: UserAuth,
+    public alertCtrl: AlertController ) {  }
 
-  }
+  // public login() {
+  //   this.auth.login(this.cred.email, this.cred.password).subscribe(allowed => {
+  //     if(allowed){
+  //       console.log("Login success?")
+  //     } else {
+  //       // this.show
+  //       console.log("Failed Login");
+  //     }
+  //   })
+  // }
 
-  public login() {
-    this.auth.login(this.cred.email, this.cred.password).subscribe(allowed => {
-      if(allowed){
-        console.log("Login success?")
-      } else {
-        // this.show
-        console.log("Failed Login");
+  login(FormLogin){
+    this.auth.login(FormLogin.value).subscribe(data => {
+      console.log(data)
+      if(data.token){
+        // this.navCtrl.setRoot(HomePage);
+        console.log('LOGGED IN!')
+      } else{
+        FormLogin.password = '';
+        let alert = this.alertCtrl.create({
+          title: 'Login Failed',
+          subTitle: data.message,
+          buttons: ['OK']
+        })
+        alert.present();
       }
     })
   }
-
-
 
 }
--- snekTrakr/src/providers/user-auth.ts
+++ snekTrakr/src/providers/user-auth.ts
@@ -1,69 +1,129 @@
+// import { Injectable } from '@angular/core';
+// import { Http, Headers, Response } from '@angular/http';
+// import 'rxjs/add/operator/map';
+// import { Observable } from 'rxjs/Observable';
+//
+// //import model User
+// import { User } from '../models/user';
+//
+//
+//
+// /*
+//   Generated class for the UserAuth provider.
+//
+//   See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+//   for more info on providers and Angular 2 DI.
+// */
+// @Injectable()
+// export class UserAuth {
+//   public token: string;
+//
+//
+//   constructor(private http: Http) {
+//     console.log('Hello UserAuth Provider');
+//     var currentUser = JSON.parse(localStorage.getItem('currentUser'));
+//     this.token = currentUser.token && currentUser.id;
+//
+//   }
+// /////
+// // public login(credentials) {
+// //     if (credentials.email === null || credentials.password === null) {
+// //       return Observable.throw("Please insert credentials");
+// //     } else {
+// //       return Observable.create(observer => {
+// //         // At this point make a request to your backend to make a real check!
+// //         let access = (credentials.password === "pass" && credentials.email === "email");
+// //         this.currentUser = new User('Simon', 'saimon@devdactic.com');
+// //         observer.next(access);
+// //         observer.complete();
+// //       });
+// //     }
+// //   }
+// ////
+//   login(email: string, password: string): Observable<boolean> {
+//     return this.http.post('https://snek-trakr.herokuapp.com/login',  JSON.stringify({email: email, password: password}))
+//     .map((response: Response) => {
+//       let token = response.json() && response.json().token;
+//       if (token) {
+//           // set token property
+//           this.token = token;
+//
+//           // store username and jwt token in local storage to keep user logged in between page refreshes
+//           localStorage.setItem('currentUser', JSON.stringify({ email: email, token: token }));
+//
+//           // return true to indicate successful login
+//           return true;
+//       } else {
+//           // return false to indicate failed login
+//           return false;
+//       }
+//     })
+//
+//   }
+//
+//   logout(): void {
+//       // clear token remove user from local storage to log user out
+//       this.token = null;
+//       localStorage.removeItem('currentUser');
+//   }
+// }
 import { Injectable } from '@angular/core';
-import { Http, Headers, Response } from '@angular/http';
+import { Http, Response, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
-import { Observable } from 'rxjs/Observable';
 
-//import model User
-import { User } from '../models/user';
 
 
 
 /*
-  Generated class for the UserAuth provider.
-
+  Generated class for the Auth provider.
   See https://angular.io/docs/ts/latest/guide/dependency-injection.html
   for more info on providers and Angular 2 DI.
 */
 @Injectable()
 export class UserAuth {
-  public token: string;
-
-
-  constructor(private http: Http) {
-    console.log('Hello UserAuth Provider');
-    var currentUser = JSON.parse(localStorage.getItem('currentUser'));
-    this.token = currentUser.token && currentUser.id;
-
-  }
-/////
-// public login(credentials) {
-//     if (credentials.email === null || credentials.password === null) {
-//       return Observable.throw("Please insert credentials");
-//     } else {
-//       return Observable.create(observer => {
-//         // At this point make a request to your backend to make a real check!
-//         let access = (credentials.password === "pass" && credentials.email === "email");
-//         this.currentUser = new User('Simon', 'saimon@devdactic.com');
-//         observer.next(access);
-//         observer.complete();
-//       });
-//     }
-//   }
-////
-  login(email: string, password: string): Observable<boolean> {
-    return this.http.post('https://snek-trakr.herokuapp.com/login',  JSON.stringify({email: email, password: password}))
-    .map((response: Response) => {
-      let token = response.json() && response.json().token;
-      if (token) {
-          // set token property
-          this.token = token;
-
-          // store username and jwt token in local storage to keep user logged in between page refreshes
-          localStorage.setItem('currentUser', JSON.stringify({ email: email, token: token }));
-
-          // return true to indicate successful login
-          return true;
-      } else {
-          // return false to indicate failed login
-          return false;
-      }
-    })
-
+  baseUrl:string = 'https://snek-trakr.herokuapp.com/';
+  constructor(public http: Http) {
+    console.log('Hello Auth Provider');
   }
 
-  logout(): void {
-      // clear token remove user from local storage to log user out
-      this.token = null;
-      localStorage.removeItem('currentUser');
+  createAuthorizationHeader(headers: Headers){
+    headers.append('Accept', 'application/json')
+    headers.append('Content-type', 'application/json');
+    headers.append('Authorization', window.localStorage.getItem('token'));
   }
+
+  private(){
+    let headers = new Headers();
+    this.createAuthorizationHeader(headers);
+    return this.http.get(this.baseUrl+'private', {
+      headers: headers
+    }).map(res => res.json());
+  }
+
+  login(data){
+    return this.http.post(this.baseUrl+"login", data)
+    .map(this.extractData);
+  }
+
+  isLogged(){
+    if(window.localStorage.getItem('token')){
+      return true
+    }else{
+      return false;
+    }
+  }
+
+  logout(){
+    window.localStorage.removeItem('token');
+    return true;
+  }
+
+  private extractData(res: Response){
+    let body = res.json();
+    if(body.succes === true){
+      window.localStorage.setItem('token', body.token);
+    };
+    return body || {};
+  }
+
 }
